b95ba0ea1364afa66432277766cd0db2dc1729f6
Joao Vale <jcarlos.vale@gmail.com>
2022-12-13 23:00:29 +0000

    fixing controller

--- spring-school-api/src/main/java/com/study/spring/view/rs/ProfessorController.java
+++ spring-school-api/src/main/java/com/study/spring/view/rs/ProfessorController.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/professores")
+@RestController
+@RequestMapping("/professores")
 @Slf4j
 public class ProfessorController {
 
