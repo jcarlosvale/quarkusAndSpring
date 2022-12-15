5c02761931a365a2cd6be7311070c72b907937cb
Joao Vale <jcarlos.vale@gmail.com>
2022-12-15 17:15:18 +0000

    quarkus database using Panache

--- quarkus-school-api/src/main/java/com/study/quarkus/model/Professor.java
+++ quarkus-school-api/src/main/java/com/study/quarkus/model/Professor.java
@@ -1,5 +1,6 @@
 package com.study.quarkus.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +14,12 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "PROFESSORES")
-public class Professor {
+public class Professor extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "professor_id")
-    private Long id;
+    private Integer id;
 
     @Column(name = "professor_name", nullable = false)
     private String name;
