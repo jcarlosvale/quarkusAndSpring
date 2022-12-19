112d2f31edb1bbdff0a5d48f727786e06f8b48ea
Joao Vale <jcarlos.vale@gmail.com>
2022-12-19 15:51:46 +0000

    fix size name of disciplina

--- quarkus-school-api/src/main/java/com/study/quarkus/model/Disciplina.java
+++ quarkus-school-api/src/main/java/com/study/quarkus/model/Disciplina.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Builder
@@ -24,7 +23,6 @@ public class Disciplina {
     private Integer id;
 
     @NotBlank(message = "Name must be not empty or null")
-    @Size(min = 4, message = "Minimum name length 4 characters")
     @Column(name = "disciplina_name", nullable = false)
     private String name;
 
